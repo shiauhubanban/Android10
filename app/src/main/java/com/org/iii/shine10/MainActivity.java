8b66dc8553e3151acefc091801c745c8188f88ab
user <a>
2016-11-18 16:29:21 +0800

    v3

--- app/src/main/java/com/org/iii/shine10/MainActivity.java
+++ app/src/main/java/com/org/iii/shine10/MainActivity.java
@@ -4,22 +4,38 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences sp;
     private SharedPreferences.Editor editor;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv = (TextView)findViewById(R.id.tv);
         sp = getSharedPreferences("gamedata",MODE_PRIVATE);
         editor = sp.edit();
     }
+
+    // 偏好設定 => save
    public void test1(View v){
-       
+       editor.putInt("stage",3);
+       editor.putString("user","shine");
+       editor.commit();
+       Toast.makeText(this,"Save OK",Toast.LENGTH_SHORT);
    }
 
+    // 偏好設定 => read
+    public void test2(View v){
+        int stage = sp.getInt("stage",0);
+        String name = sp.getString("user","nobody");
+        tv.setText("Stage: " + stage + "\n" +
+                "User: " + name);
+    }
 
 }
