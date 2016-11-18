6b25b52510edbaaa110c1d8b8cda392543359b37
user <a>
2016-11-18 16:07:54 +0800

    v2

--- app/src/main/java/com/org/iii/shine10/MainActivity.java
+++ app/src/main/java/com/org/iii/shine10/MainActivity.java
@@ -1,13 +1,25 @@
 package com.org.iii.shine10;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences sp;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp = getSharedPreferences("gamedata",MODE_PRIVATE);
+        editor = sp.edit();
     }
+   public void test1(View v){
+       
+   }
+
+
 }
