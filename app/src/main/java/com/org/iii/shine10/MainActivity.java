3abd2c27736081db384d3a1874815c89bed67f0e
user <a>
2016-11-21 09:50:33 +0800

    v5

--- app/src/main/java/com/org/iii/shine10/MainActivity.java
+++ app/src/main/java/com/org/iii/shine10/MainActivity.java
@@ -8,9 +8,11 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences sp;
@@ -46,18 +48,31 @@ public class MainActivity extends AppCompatActivity {
     public void test3(View v){
         // 在 內存空間之下的 FileSystem
         try {
-            FileOutputStream fout = openFileOutput("Shine data",MODE_PRIVATE);
+            FileOutputStream fout = openFileOutput("Shine.data",MODE_PRIVATE);
             fout.write("Hello Shine".getBytes());
             fout.flush();
             fout.close();
+            Toast.makeText(this,"save ok",Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
             e.printStackTrace();
-            Log.v("shine",e.toString());
+            Log.v("shine","test03:"+ e.toString());
         }
     }
     //內存 read
     public void test4(View v){
-
+        tv.setText("");
+        try {
+            BufferedReader reader =
+                    new BufferedReader(
+                    new InputStreamReader(openFileInput("Shine.data")));
+            String line;
+            while ((line = reader.readLine())!=null) {
+                tv.append(line + "\n");
+            }
+            reader.close();
+        } catch (IOException e) {
+            Log.v("shine","test04:"+ e.toString());
+        }
     }
 
 
